daeda04e7d42537f086602c3fa51a2c02d3026e7
yango94 <yzong17@gmail.com>
2023-09-07 15:58:46 -0700

    post section

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-# Greets user
+# Uses a single route
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name", "world"))
     return render_template("index.html")
-
-
-@app.route("/greet")
-def greet():
-    return render_template("greet.html", name=request.args.get("name", "world!"))
